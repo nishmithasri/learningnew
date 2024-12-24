4f5ff4d04b09e40035d0a357889489e589a222e0
nishmithasri <nishmitha.ramesh26@gmail.com>
2024-12-24 07:24:45 +0000

    Basic dropdown

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("learningnew.controller.View1", {
+        onInit() {
+        },
+        onOK: function(){
+            
+        }
+    });
+});
